3702187bcb81e69cfcb85d888b315c79097dad79
Thais Ribeiro <thaisribeiro@softbox.com.br>
2022-09-03 15:58:23 -0300

    second

--- python-I/cart.py
+++ python-I/cart.py
@@ -1,20 +1,18 @@
-carrinho = []
+cart = []
 
-def add_item_cart(item):
-    carrinho.append(item)
-    return carrinho
+id_user = input("Insira o id do usuário:")
+id_produto = input("Insira o id do produto:")
+price_product = float(input("Insira o preço do produto:"))
+quantity_product = int(input("Insira a quantidade de produto:"))
 
-def get_all_item_cart():
-    return carrinho
-    
-def execute():
-    id_user = input("Insira o id do usuário:")
-    id_produto = input("Insira o id do produto:")
-    price_product = float(input("Insira o preço do produto:"))
-    quantity_product = int(input("Insira a quantidade de produto:"))
-    item = [id_produto, id_user, price_product, quantity_product]
-    carrinho = add_item_cart(item)
-    print(carrinho)
+# id produto, id usuario, preço, quantidade
+item = [id_produto, id_user, price_product, quantity_product]
+def add_item_cart():
+    cart.append(item)
+    return cart
 
-execute()
-
+def get_all_items_cart():
+    return cart
+def get_item_cart_by_product(id_product):
+    new_list = [item for item in cart if item[0] == id_product]
+    return new_list[0]
